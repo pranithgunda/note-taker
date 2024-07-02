58f5d79e4c722c7bc9ab1813e0e84191c2af2f18
Pranith Gunda <pranith0527@yahoo.com>
2024-07-02 11:10:13 -0400

    Modified server.js file to fetch index.html page on base URL

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 // middleware to serve static files from public directory
 app.use(express.static('public'));
 
-// to fetch notes.html
+// to fetch index.html file as home page
+app.get('/',(req,res)=>
+res.sendFile(path.join(__dirname,'/public/index.html')))
+
+// to fetch notes.html file
 app.get('/notes',(req,res)=>
 res.sendFile(path.join(__dirname,'/public/notes.html')))
 
