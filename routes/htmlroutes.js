c41fe0fea0f594e6f1c59a00b75208e85e603314
Pranith Gunda <pranith0527@yahoo.com>
2024-07-04 18:06:42 -0400

    Optimized routing to be modular, redefined routes to handle get and post methods for html and json responses, deleted unused files and made code simpler, added commenys

---
+++ routes/htmlroutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const path = require('path')
+const currentDirectory = __dirname;
+// Navigate up one level in directory structure, to effectively get the base directory path
+const baseDirectory = path.resolve(currentDirectory,'..')
+
+// get route for the notes page
+router.get('/notes', (req, res) =>
+// Use path.join to return single normalized path
+res.sendFile(path.join(baseDirectory,'public/notes.html')))
+
+
+// route to home page for wildchar 
+router.get('*', (req, res) =>
+res.sendFile(path.join(baseDirectory, 'public/index.html')))
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 // Install all required dependencies
 const express = require('express');
-const path=require('path');
+
+// variables created for routes to api and html
+const apiRoutes = require('./routes/apiroutes')
+const htmlRoutes = require('./routes/htmlroutes')
+
 
 // PORT variable
 const PORT = process.env.PORT || 3001;
 // app variable to use express methods
 const app = express();
-// api variable created for routes
-const api = require('./routes/index')
+
+// middleware for parsing json and url encoded form data
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 // middleware to serve static files from public directory
 app.use(express.static('public'));
 
-// get route for the home page
-app.get('/',(req,res)=>
-res.sendFile(path.join(__dirname,'/public/index.html')))
-
-// get route for the notes page
-app.get('/notes',(req,res)=>
-res.sendFile(path.join(__dirname,'/public/notes.html')))
-
-// Send all the requests that begin with /api to the index.js in the routes folder
-app.use('/api',api);
+// Send all the requests that begin with /api to the apiroutes in the routes folder
+app.use('/api',apiRoutes);
+// Send all the requests with base URL to htmlroutes in the routes folder 
+app.use('/',htmlRoutes);
 
 app.listen(PORT,()=>console.log(`App listening on PORT ${PORT}`));
