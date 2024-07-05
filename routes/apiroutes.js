e97ff271941121b54692ce91e66a7d17aec9cd7e
Pranith Gunda <pranith0527@yahoo.com>
2024-07-05 00:41:48 -0400

    Added route to delete a note, enhanced exceptional handling for all routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,15 +2,21 @@ const router = require('express').Router();
 //package to generate unique id
 const generateUniqueID = require('generate-unique-id');
 // Destructure the methods from fsutils
-const { readFromFile, readAndWriteToFile } = require('../utils/fsutils')
+const { readFromFile, readAndWriteToFile, deleteNoteFromFile } = require('../utils/fsutils')
 
 
 // route defined to provide notes available
 router.get('/notes', (req, res) => {
     readFromFile('./db/db.json')
         .then((data) => {
-            // Use res.json to send json data back to client
-            res.status(200).json(JSON.parse(data))
+            if (data.length > 0) {
+                // Use res.json to send json data back to client
+                res.status(200).json(JSON.parse(data))
+            }
+            else {
+                // If file is empty return success with message
+                res.status(200).json({ message: 'Notes added are empty' })
+            }
         })
         .catch(error => {
             console.error('An error occured while reading file db.json data', error);
@@ -24,6 +30,7 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const { title, text } = req.body;
     if (title && text) {
+        // Construct new note with entered information to save
         const newNote = {
             title,
             text,
@@ -32,6 +39,7 @@ router.post('/notes', (req, res) => {
                 useLetter: true
             })
         }
+        // function to read entered notes and write to file with appended note
         readAndWriteToFile('./db/db.json', newNote)
             .then(result => res.status(200).json(result))
             .catch(error => {
@@ -40,9 +48,36 @@ router.post('/notes', (req, res) => {
             })
     }
     else {
-        res.status(400).json('Please enter all details to add a note');
+        res.status(400).json({ 'error': 'Please enter all details to add a note' });
     }
 
+})
+
+//route defined to delete a note
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    if (id) {
+        deleteNoteFromFile('./db/db.json', id)
+            .then(result => {
+                if (result.statusCode === '200') {
+                    // Have to pass status code value instead of property reference to avoid express warning
+                    res.status(200).json(result)
+                }
+                else if (result.statusCode === '404') {
+                    res.status(404).json(result)
+                } else {
+                    res.json(result)
+                }
+            }
+            )
+            .catch(error => {
+                console.error('An error occured while deleting note', error);
+                res.status(500).json({ 'error': 'error occured while deleting note' })
+            })
+
+    } else {
+        res.status(400).json({ 'error': 'Provide note id for deletion' })
+    }
 })
 
 module.exports = router;
